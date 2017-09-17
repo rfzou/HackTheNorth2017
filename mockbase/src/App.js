44c1d94c4e7ba757301d1bbde3f7a73a17ea84ab
rrrtt <rickietruong@gmail.com>
2017-09-17 09:11:19 -0400

    lander

--- mockbase/src/App.js
+++ mockbase/src/App.js
@@ -5,21 +5,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      
       <div className="App">
         <div className="App-header">
           <img src="images/logo.png" className="img-responsive center-block" id="logo" />
         </div>
-          <div className="App-input form-group">
+          <div className="form-group">
             <input type="login" className="form-control input-underline input-lg" placeholder="Email" /> 
           </div>
-          <div className="App-input form-group">
+          <div className="form-group">
             <input type="password" className="form-control input-underline input-lg" placeholder="Password" /> 
           </div>
-          {/* <div className="input-group">
-            <span className="input-group-addon" id="basic-addon1">@</span>
-            <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
-          </div> */}
+          <label className="form-check-label">
+            <button type="submit" className="btn btn-primary btn-block">Sign In</button>
+          </label>
       </div>
     );
   }
